1bb9f587ea2b4dc3f2de4083ab187f5b4d96966d
ellec2 <ecohen1@exeter.edu>
2023-01-06 18:36:48 -0500

    claw code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Claw.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import java.util.*;
+
+//import com.qualcomm.robotcore.hardware.opmode;
+//import com.qualcomm.robotcore.hardware.opmode.Autonomous;
+
+@Autonomous(name = "Claw")
+public class Claw {
+
+    private DcMotor frontleft;
+    private DcMotor frontright;
+    private DcMotor backright;
+    private DcMotor backleft;
+
+    private Servo leftServo;
+    private Servo rightServo;
+
+
+    public void init(HardwareMap hardwareMap) {
+        frontleft = hardwareMap.get(DcMotor.class, "frontleft");
+        frontright = hardwareMap.get(DcMotor.class, "frontright");
+        backright = hardwareMap.get(DcMotor.class, "backright");
+        backleft = hardwareMap.get(DcMotor.class, "backleft");
+    }
+
+
+}
