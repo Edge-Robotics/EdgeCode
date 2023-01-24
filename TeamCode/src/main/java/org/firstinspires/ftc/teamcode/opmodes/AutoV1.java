d80f3f3abcf581a21c7ae90792d8cb9cc81599d1
Stanley <62864388+shuang-0608@users.noreply.github.com>
2023-01-24 17:05:15 -0500

    ftc dashboard

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutoV1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutoV1.java
@@ -22,6 +22,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.subsystems.Intake;
 import org.firstinspires.ftc.teamcode.subsystems.Elevator;
+import org.firstinspires.ftc.teamcode.subsystems.AutoMechanum;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.subsystems.Mechanum;
 
@@ -49,7 +50,7 @@ public class AutoV1 extends LinearOpMode {
     ColorSensor colorSensor;
 
     private Intake mainClaw = new Intake();
-    //private AutoMechanum mainAutoMechanum = new AutoMechanum();
+    private AutoMechanum mainAutoMechanum = new AutoMechanum();
     private Elevator mainElevator = new Elevator();
     private DcMotorEx elevatorLeft = null;
     private DcMotorEx elevatorRight = null;
@@ -88,35 +89,16 @@ public class AutoV1 extends LinearOpMode {
         mainClaw.init(hardwareMap);
         mainElevator.init(hardwareMap);
 
-        //colorSensor = hardwareMap.get(ColorSensor.class, "ColorSensor");
+        colorSensor = hardwareMap.get(ColorSensor.class, "ColorSensor");
 
 
 
 
             waitForStart();
-            while (opModeIsActive())
-            {
-                if (reset == false){
-                    timer.reset();
-                    reset = true;
-                    mainElevator.setTargetPosition(slightPosition);
-                }
-                if (timer.seconds() < 2.25) {
-                    frontLeft.setPower(-0.5);
-                    frontRight.setPower(0.5);
-                    backRight.setPower(0.5);
-                    backLeft.setPower(-0.5);
-                }
-                else {
-                    frontLeft.setPower(0);
-                    frontRight.setPower(0);
-                    backRight.setPower(0);
-                    backLeft.setPower(0);
-                    mainElevator.setTargetPosition(bottomPosition);
-                break;
-            }
-            }
-                /*telemetry.addData("Robot Status", "Initialized");
+            while (opModeIsActive()) {
+
+
+                telemetry.addData("Robot Status", "Initialized");
                 telemetry.addData("Elevator Position: ", elevatorLeft.getCurrentPosition());
                 telemetry.addData("Front Left: ", frontLeft.getCurrentPosition());
                 telemetry.addData("Front Right: ", frontRight.getCurrentPosition());
@@ -128,75 +110,71 @@ public class AutoV1 extends LinearOpMode {
                 telemetry.addData("Blue", colorSensor.blue());
                 yellow = false;
                 magenta = false;
-                cyan = false;*/
+                cyan = false;
 
-//                mainClaw.setIntakePower(1);
-//                sleep(500);
-//                mainClaw.setIntakePower(0);
-//                sleep(500);
-//                mainElevator.setTargetPosition(slightPosition);
-//                sleep(500);
-//                mainAutoMechanum.driveForward(200,500);
-//                mainAutoMechanum.brake();
-//                sleep(500);
-//                mainElevator.setTargetPosition(topPosition);
-//                sleep(500);
-//                mainAutoMechanum.driveForward(50,100);
-//                sleep(500);
-//                mainClaw.setIntakePower(-1);
-//                sleep(500);
-//                mainClaw.setIntakePower(0);
-//                sleep(500);
-//                mainAutoMechanum.driveBackward(100,200);
-//                sleep(500);
-//                mainElevator.setTargetPosition(bottomPosition);
+                mainClaw.setIntakePower(1);
+                sleep(500);
+                mainClaw.setIntakePower(0);
+                sleep(500);
+                mainElevator.setTargetPosition(slightPosition);
+                sleep(500);
+                mainAutoMechanum.driveForward(200, 500);
+                mainAutoMechanum.brake();
+                sleep(500);
+                mainElevator.setTargetPosition(topPosition);
+                sleep(500);
+                mainAutoMechanum.driveForward(50, 100);
+                sleep(500);
+                mainClaw.setIntakePower(-1);
+                sleep(500);
+                mainClaw.setIntakePower(0);
+                sleep(500);
+                mainAutoMechanum.driveBackward(100, 200);
+                sleep(500);
+                mainElevator.setTargetPosition(bottomPosition);
 
 
-//                sleep(500);
-//                mainAutoMechanum.driveBackward(200,500);
-//                sleep(500);
-//                mainAutoMechanum.turnLeft(200,500); // MAKE FUNCTION FOR 90 DEGREE TURNS (OR TURNS OF X DEGREES IN GENERAL)
-//                sleep(500);
-//                mainAutoMechanum.turnRight(200,500);
-//                sleep(500);
-//                mainAutoMechanum.strafeLeft(200,500);
-//                sleep(500);
-//                mainAutoMechanum.strafeRight(200,500);
-//                sleep(500);
-//                mainElevator.setTargetPosition(topPosition);
-//                sleep(500);
-//                mainElevator.setTargetPosition(bottomPosition);
-//                sleep(500);
-//                mainClaw.intake();
-//                sleep(500);
-//                mainClaw.setIntakePower(0);
-//                sleep(500);
-//                mainClaw.outTake();
-               // mainClaw.setIntakePower(0);
-                //
-                /*
-                if (colorSensor.green() > colorSensor.blue() && colorSensor.green() > colorSensor.red() && colorSensor.green() - colorSensor.blue() > 0.05 && colorSensor.green() - colorSensor.red() > 0.05){
+                sleep(500);
+                mainAutoMechanum.driveBackward(200, 500);
+                sleep(500);
+                mainAutoMechanum.turnLeft(200, 500); // MAKE FUNCTION FOR 90 DEGREE TURNS (OR TURNS OF X DEGREES IN GENERAL)
+                sleep(500);
+                mainAutoMechanum.turnRight(200, 500);
+                sleep(500);
+                mainAutoMechanum.strafeLeft(200, 500);
+                sleep(500);
+                mainAutoMechanum.strafeRight(200, 500);
+                sleep(500);
+                mainElevator.setTargetPosition(topPosition);
+                sleep(500);
+                mainElevator.setTargetPosition(bottomPosition);
+                sleep(500);
+                mainClaw.intake();
+                sleep(500);
+                mainClaw.setIntakePower(0);
+                sleep(500);
+                mainClaw.outTake();
+                mainClaw.setIntakePower(0);
+
+                if (colorSensor.green() > colorSensor.blue() && colorSensor.green() > colorSensor.red() && colorSensor.green() - colorSensor.blue() > 0.05 && colorSensor.green() - colorSensor.red() > 0.05) {
                     yellow = true;
-                }
-                else if (colorSensor.blue() > colorSensor.red() && colorSensor.green() > colorSensor.red() && colorSensor.blue() - colorSensor.red() > 0.05){
+                } else if (colorSensor.blue() > colorSensor.red() && colorSensor.green() > colorSensor.red() && colorSensor.blue() - colorSensor.red() > 0.05) {
                     cyan = true;
-                }
-                else if (Math.abs(colorSensor.blue() - colorSensor.red()) < 0.06 && Math.abs(colorSensor.red() - colorSensor.green()) < 0.06 && Math.abs(colorSensor.green() - colorSensor.blue()) > 0.06){
+                } else if (Math.abs(colorSensor.blue() - colorSensor.red()) < 0.06 && Math.abs(colorSensor.red() - colorSensor.green()) < 0.06 && Math.abs(colorSensor.green() - colorSensor.blue()) > 0.06) {
                     magenta = true;
                 }
 
-                if (yellow){
+                if (yellow) {
                     telemetry.addData("Current Color Detected", "Yellow");
-                }
-                else if (cyan){
+                } else if (cyan) {
                     telemetry.addData("Current Color Detected", "Cyan");
-                }
-                else if (magenta){
+                } else if (magenta) {
                     telemetry.addData("Current Color Detected", "Magenta");
-                }
-                else{
+                } else {
                     telemetry.addData("Current Color Detected", "Unknown");
-                }*/
+                }
+                break;
+            }
 
 
 
